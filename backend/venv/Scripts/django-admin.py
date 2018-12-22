543d51a3a3300486804e62b7025371ebc063bb4b
Diego Osorio <DieOsorio@Outlook.com>
2018-12-22 17:06:25 -0300

    initial commit

---
+++ backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dieos\onedrive\source\react-django\djreact\backend\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
